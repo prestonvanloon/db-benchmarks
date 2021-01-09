3e7fbf191992fab5ba62820b1b9f33a44fcbe0fa
Preston Van Loon <preston@prysmaticlabs.com>
2021-01-08 16:43:18 -0800

    Trying benchmarks with WAL

--- bolt_wal/bolt_wal_test.go
+++ bolt_wal/bolt_wal_test.go
@@ -1,6 +1,7 @@
-package boltBenchmark
+package boltWalBenchmark
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"path"
@@ -9,10 +10,11 @@ import (
 	"time"
 
 	. "github.com/rawfalafel/db-benchmarks"
+	"github.com/tidwall/wal"
 	bolt "go.etcd.io/bbolt"
 )
 
-func setupBolt(t *testing.T, noSync bool) *bolt.DB {
+func setupBolt(t *testing.T, noSync bool) (*bolt.DB, *wal.Log) {
 	datadir := SetupDir("bolt", t)
 	datafile := path.Join(datadir, "bolt.db")
 
@@ -23,25 +25,58 @@ func setupBolt(t *testing.T, noSync bool) *bolt.DB {
 		t.Fatalf("failed to open db: %v", err)
 	}
 
-	return db
+	log, err := wal.Open(path.Join(datadir, "wal.db"), &wal.Options{
+		NoCopy: true,
+	})
+	if err != nil {
+		t.Fatalf("failed to open wal db: %v", err)
+	}
+
+	return db, log
 }
 
-func _randomWrite(db *bolt.DB, r *rand.Rand) error {
+func _randomWrite(log *wal.Log, r *rand.Rand) error {
 	k, v := GenerateKV(r)
+	d := append(k, v...)
+	i, err := log.LastIndex()
+	if err != nil {
+		return err
+	}
+	return log.Write(i+1, d)
+}
 
+func _flush(db *bolt.DB, log *wal.Log) error {
 	err := db.Update(func(tx *bolt.Tx) error {
+		end, err := log.LastIndex()
+		if err != nil {
+			return err
+		}
 		b, err := tx.CreateBucketIfNotExists([]byte("MyBucket"))
 		if err != nil {
 			return fmt.Errorf("create bucket: %v", err)
 		}
-
-		return b.Put(k, v)
+		if end <= 0 {
+			return errors.New("no entries to write")
+		}
+		for i := uint64(0); i <= end; i++ {
+			d, err := log.Read(i)
+			if err != nil {
+				return err
+			}
+			if len(d) != 332 { // test assumption
+				return errors.New("data is incorrect length")
+			}
+			k, v := d[:31], d[32:]
+			if err := b.Put(k, v); err != nil {
+				return err
+			}
+		}
+		return nil
 	})
 	if err != nil {
 		return fmt.Errorf("update failed: %v", err)
 	}
-
-	return nil
+	return log.TruncateBack(0)
 }
 
 func _batchWrite(db *bolt.DB, r *rand.Rand) error {
@@ -84,17 +119,26 @@ func _randomRead(db *bolt.DB, r *rand.Rand) error {
 	return nil
 }
 
-func randomWrite(t *testing.T, db *bolt.DB, size int) {
+func randomWrite(t *testing.T, db *bolt.DB, log *wal.Log, size int) {
 	s := rand.NewSource(1)
 	r := rand.New(s)
+	start := time.Now()
 	for i := 0; i < size; i++ {
-		if err := _randomWrite(db, r); err != nil {
+		if err := _randomWrite(log, r); err != nil {
 			t.Fatal(err)
 		}
 	}
+	if i, err := log.LastIndex(); err != nil {
+		t.Fatal(err)
+	} else {
+		t.Logf("There are %d keys in WAL", i)
+	}
+	TrackTime(start, "wal entries")
+	_flush(db, log)
 }
 
-func randomRead(t *testing.T, db *bolt.DB, size int) {
+func randomRead(t *testing.T, db *bolt.DB, log *wal.Log, size int) {
+	// TODO: Put some keys in the log to scan.
 	s := rand.NewSource(1)
 	r := rand.New(s)
 	for i := 0; i < size; i++ {
@@ -104,7 +148,7 @@ func randomRead(t *testing.T, db *bolt.DB, size int) {
 	}
 }
 
-func concurrentWrite(t *testing.T, db *bolt.DB, size, partitions int) {
+func concurrentWrite(t *testing.T, db *bolt.DB, log *wal.Log, size, partitions int) {
 	wg := sync.WaitGroup{}
 	sizePerPartition := size / partitions
 	var batchErr error
@@ -142,27 +186,27 @@ func concurrentWrite(t *testing.T, db *bolt.DB, size, partitions int) {
 }
 
 func TestBoltWrite(t *testing.T) {
-	db := setupBolt(t, false)
+	db, log := setupBolt(t, false)
 	defer db.Close()
-	defer TrackTime(time.Now(), "bolt write")
+	defer TrackTime(time.Now(), "wal write + flush + bolt write")
 
-	randomWrite(t, db, 1<<17)
+	randomWrite(t, db, log, 1<<17)
 }
 
-func TestBoltRead(t *testing.T) {
-	db := setupBolt(t, true)
-	defer db.Close()
+// func TestBoltRead(t *testing.T) {
+// 	db, log := setupBolt(t, true)
+// 	defer db.Close()
 
-	randomWrite(t, db, 1<<20)
-	defer TrackTime(time.Now(), "bolt read")
+// 	randomWrite(t, db, 1<<20)
+// 	defer TrackTime(time.Now(), "bolt read")
 
-	randomRead(t, db, 1<<20)
-}
+// 	randomRead(t, db, log, 1<<20)
+// }
 
 func TestBoltConcurrentWrite(t *testing.T) {
-	db := setupBolt(t, false)
+	db, log := setupBolt(t, false)
 	defer db.Close()
 
 	defer TrackTime(time.Now(), "bolt concurrent write")
-	concurrentWrite(t, db, 1<<17, 1<<12)
+	concurrentWrite(t, db, log, 1<<17, 1<<12)
 }
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rawfalafel/db-benchmarks
+
+go 1.15
+
+require (
+	github.com/dgraph-io/badger v1.6.2
+	github.com/syndtr/goleveldb v1.0.0
+	github.com/tidwall/wal v0.1.4
+	go.etcd.io/bbolt v1.3.5
+)
